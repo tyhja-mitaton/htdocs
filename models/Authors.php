1a2da753b10a9526b9fd7273e5a0ade8b83c1b3d
void45000 <ltbr@bk.ru>
2019-03-01 11:10:11 +0500

    Initial commit

---
+++ models/Authors.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "authors".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $book_quant
+ */
+class Authors extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'authors';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'book_quant'], 'required'],
+            [['book_quant'], 'integer'],
+            [['name'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'ФИО',
+            'book_quant' => 'Количество книг',
+        ];
+    }
+
+}
---
+++ models/Books.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "books".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $price
+ * @property int $author
+ */
+class Books extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'books';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price'], 'required'],
+            [['price', 'author'], 'integer'],
+            [['name'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'price' => 'Price',
+            'author' => 'Author',
+        ];
+    }
+
+    public function getAuthor(){
+        return $this->hasOne(Authors::className(),['id'=>'author']);
+    }
+}
